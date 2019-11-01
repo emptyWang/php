548386015ccd112c94fe428447456c915ceeed35
wangzezhong <wangzezhong@zuoyebang.com>
2019-11-01 11:21:36 +0800

    clean code

--- index.php
+++ index.php
@@ -2,27 +2,38 @@
 
 /**
  * How many life at that time?
+ *
+ * Tip: when time is coming, it split then die
+ *
+ * @param     $durationHours
+ * @param int $startNumber
+ * @param int $lifeHours
+ * @param int $fissionTime
+ *
+ * @return float
  */
+function howManyAtThatTime($durationHours, $startNumber = 1, $lifeHours = 3, $fissionTime = 1): float
+{
+    $total = $startNumber;
 
-//init number
-$startNumber = $total = 1;
-//one lift time
-$lifeHours = 3;
-//duration time
-$durationHours = 6;
-//fission interval time
-$fissionTime = 1;
+    $needDieNumber             = [];
+    $count                     = 1;
+    $needDieNumber[$lifeHours] = $startNumber;
+    while ($durationHours--) {
+        $new = 0;
+        if ($count % $fissionTime === 0) {
+            $new = $total;
+        }
+        $dieNumber                          = isset($needDieNumber[$count]) ? $needDieNumber[$count] : 0;
+        $total                              = $total + $new - $dieNumber;
+        $needDieNumber[$count + $lifeHours] = $new;
+        echo ( $count ) . "hours\t total:" . $total . "\n";
+        $count++;
+    }
 
-$needDieNumber             = [];
-$count                     = 1;
-$needDieNumber[$lifeHours] = $startNumber;
-while ($durationHours --) {
-    $new                                = $total;
-    $dieNumber                          = isset($needDieNumber[$count]) ? $needDieNumber[$count] : 0;
-    $total                              = $total + $new - $dieNumber;
-    $needDieNumber[$count + $lifeHours] = $new;
-    echo ( $count ) . "hours\t total:" . $total . "\n";
-    $count++;
+    return $total;
 }
+
+echo howManyAtThatTime(7);
 
 
